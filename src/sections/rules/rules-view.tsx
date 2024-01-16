a8b89b848a8914fabb17be5efa9236f0b395fde6
florousseau <florent.rousseau@student.uclouvain.be>
2024-01-16 22:30:19 +0100

    :bug: Added read from data for the rules and fixed Readm

--- src/sections/rules/rules-view.tsx
+++ src/sections/rules/rules-view.tsx
@@ -1,30 +1,46 @@
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import Card from "@mui/material/Card";
-import { CardContent } from "@mui/material";
+import {CardContent} from "@mui/material";
+import Loading from "../loading/loading.tsx";
+import Container from "@mui/material/Container";
+import {useData} from "../../data/DataProvider.tsx";
+import {DocumentData} from "@firebase/firestore";
+
+
+interface DataContextValue {
+  data: DocumentData | null;
+  refetchData: () => void;
+}
 
 export default function Rules() {
 
-  return (
-    <>
-      <Stack
-        direction="row"
-        alignItems="center"
-        justifyContent="space-between"
-        mb={5}
-      >
-        <Typography variant="h4" sx={{ mb: 1 }}>
-          Le Règlement
-        </Typography>
-      </Stack>
+    const {data} = useData() as DataContextValue;
 
-      <Card sx={{ width: "100%", mb: 4 }}>
-        <CardContent>
-            Sync règlement from the db, start and stop dates also sync from the db <br />
-            Toute enfreinte au règlment sera punis d'un affond de périmé en 15n* <br />
-            *sous réserve de stock
-        </CardContent>
-      </Card>
-    </>
-  );
+    return (
+        <>
+            <Stack
+                direction="row"
+                alignItems="center"
+                justifyContent="space-between"
+                mb={5}
+            >
+                <Typography variant="h4" sx={{mb: 1}}>
+                    Le Règlement
+                </Typography>
+            </Stack>
+
+            <Card sx={{width: "100%", mb: 4}}>
+                <CardContent>
+                    <Container>
+                        { data ? (
+                            data.data().rules
+                        ) : (
+                            <Loading />
+                        ) }
+                    </Container>
+                </CardContent>
+            </Card>
+        </>
+    );
 }
