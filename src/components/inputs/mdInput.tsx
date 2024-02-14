9775a289be82029f84ab571aefaabef9938d2658
florousseau <florent.rousseau@student.uclouvain.be>
2024-02-14 16:28:44 +0100

    :bug: fixed ui bug with strange line in the middle of the screen

--- src/components/inputs/mdInput.tsx
+++ src/components/inputs/mdInput.tsx
@@ -18,7 +18,7 @@ import {
 import '@mdxeditor/editor/style.css';
 
 const StyledMDXEditor = styled(MDXEditor)(({theme}) => ({
-    borderRadius: theme.shape.borderRadius,
+    /*borderRadius: theme.shape.borderRadius,
     border: `1px solid ${theme.palette.divider}`,
     "&:hover": {
         borderColor: theme.palette.action.hoverBorder,
@@ -27,7 +27,7 @@ const StyledMDXEditor = styled(MDXEditor)(({theme}) => ({
     "&:focus-within": {
         border: `2px solid ${theme.palette.primary.main}`,
         borderRadius: theme.shape.borderRadius + "px",
-    },
+    },*/
 
     "& [role=textbox]": {
         fontFamily: "Public Sans, sans-serif",
@@ -49,36 +49,51 @@ const StyledMDXEditor = styled(MDXEditor)(({theme}) => ({
 
 }));
 
+const StyledMDXEditorWrapper = styled('div')(({theme}) => ({
+    borderRadius: theme.shape.borderRadius,
+    border: `1px solid ${theme.palette.divider}`,
+    "&:hover": {
+        borderColor: theme.palette.action.hoverBorder,
+    },
+
+    "&:focus-within": {
+        border: `2px solid ${theme.palette.primary.main}`,
+        borderRadius: theme.shape.borderRadius + "px",
+    },
+}));
+
 
 const EditorWithTheme = ({setRules, markdown}: {
     setRules: (value: string) => void,
     markdown: string,
 }) => (
-    <StyledMDXEditor
-        onChange={(e) => setRules(e)}
-        plugins={[
-            tablePlugin(),
-            listsPlugin(),
-            headingsPlugin(),
-            linkDialogPlugin(),
-            thematicBreakPlugin(),
-            toolbarPlugin({
-                toolbarContents: () => (
-                    <>
-                        {' '}
-                        <UndoRedo/>
-                        <BlockTypeSelect/>
-                        <BoldItalicUnderlineToggles/>
-                        <ListsToggle/>
-                        <InsertTable/>
-                        <CreateLink/>
-                        <InsertThematicBreak/>
-                    </>
-                )
-            })
-        ]}
-        markdown={markdown}
-    />
+    <StyledMDXEditorWrapper>
+        <StyledMDXEditor
+            onChange={(e) => setRules(e)}
+            plugins={[
+                tablePlugin(),
+                listsPlugin(),
+                headingsPlugin(),
+                linkDialogPlugin(),
+                thematicBreakPlugin(),
+                toolbarPlugin({
+                    toolbarContents: () => (
+                        <>
+                            {' '}
+                            <UndoRedo/>
+                            <BlockTypeSelect/>
+                            <BoldItalicUnderlineToggles/>
+                            <ListsToggle/>
+                            <InsertTable/>
+                            <CreateLink/>
+                            <InsertThematicBreak/>
+                        </>
+                    )
+                })
+            ]}
+            markdown={markdown}
+        />
+    </StyledMDXEditorWrapper>
 );
 
 export default EditorWithTheme;
